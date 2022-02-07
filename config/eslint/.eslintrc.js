97e5c1b8865ee36de2c8b7628f65317abb014507
arthursvpb <arthurandroid15@gmail.com>
2022-02-07 16:22:10 -0300

    add nextjs initial template

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'google',
-    'prettier'
-  ],
-  plugins: ['react', '@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {}
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'next/core-web-vitals',
+		'google',
+		'prettier'
+	],
+	plugins: ['react', '@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	rules: {
+		'require-jsdoc': 0
+	}
 };
