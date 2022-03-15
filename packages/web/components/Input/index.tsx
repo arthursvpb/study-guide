c810faf63747e49b48e0271fa861a7de0ec72941
arthursvpb <arthurandroid15@gmail.com>
2022-03-15 20:25:23 -0300

    add unform input component

---
+++ packages/web/components/Input/index.tsx
@@ -0,0 +1,52 @@
+import { InputHTMLAttributes, useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+import styles from './styles.module.scss';
+
+interface InputProps {
+	name: string;
+	title: string;
+	placeholder: string;
+	icon: string;
+	type: string;
+}
+
+export function Input({
+	name,
+	title,
+	placeholder,
+	icon,
+	type,
+	...rest
+}: InputProps) {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const { fieldName, defaultValue = '', registerField, error } = useField(name);
+
+	useEffect(() => {
+		registerField({
+			name: fieldName,
+			ref: inputRef.current,
+			path: 'value'
+		});
+	}, [fieldName, registerField]);
+
+	return (
+		<>
+			{/* <img src={icon} /> */}
+
+			<p>{title}</p>
+
+			<input
+				ref={inputRef}
+				id={fieldName}
+				defaultValue={defaultValue}
+				name={name}
+				placeholder={placeholder}
+				type={type}
+				{...rest}
+			/>
+
+			{error && <span style={{ color: '#f00' }}>{error}</span>}
+		</>
+	);
+}
--- packages/web/pages/signup/index.tsx
+++ packages/web/pages/signup/index.tsx
@@ -1,9 +1,20 @@
+import { Input } from '../../components/Input';
+
 import styles from './styles.module.scss';
 
 export default function SignUp() {
 	return (
 		<div className={styles.signUpContainer}>
 			<h1>Welcome. Sign up now!</h1>
+			<p>Start planning your studies.</p>
+
+			<Input
+				name="name"
+				title="Full Name"
+				placeholder="Ex.: John Doe"
+				type="text"
+				icon="imagem"
+			/>
 		</div>
 	);
 }
