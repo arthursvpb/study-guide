1f7af81d4e84a0f8746f9cada58100aab0643123
arthursvpb <arthurandroid15@gmail.com>
2022-03-17 20:55:48 -0300

    dashboard - create first goal page

--- packages/web/pages/dashboard/index.tsx
+++ packages/web/pages/dashboard/index.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from './styles.module.scss';
 
 export default function Dashboard() {
 	return (
 		<div className={styles.dashboardContainer}>
 			<div className={styles.dashboard}>
-				<h1>Dashboard</h1>
+				<h1>Hello, Arthur Vasconcellos.</h1>
+				<p>Create your first goal now.</p>
+
+				<div className={styles.createGoal}>
+					<img src="/images/undraw_add_post_re_174w.svg" alt="Add new goal" />
+				</div>
 			</div>
 		</div>
 	);
